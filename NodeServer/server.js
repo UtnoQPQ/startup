408f069d826669ff6bb0c08d095917e6feecdee2
Salvador Olivera Iguini <UtnoQPQ@users.noreply.github.com>
2016-05-20 08:13:56 -0300

    Add files via upload

---
+++ NodeServer/server.js
@@ -0,0 +1,313 @@
+var movies = [{
+  Title: "Locke",
+  Year: "2013",
+  Rated: "R",
+  Released: "18 Apr 2014",
+  Runtime: "85 min",
+  Genre: "Drama",
+  Director: "Steven Knight",
+  Writer: "Steven Knight",
+  Actors: "Tom Hardy, Olivia Colman, Ruth Wilson, Andrew Scott",
+  Plot: "Ivan Locke, a dedicated family man and successful construction manager, receives a phone call on the eve of the biggest challenge of his career that sets in motion a series of events that threaten his carefully cultivated existence.",
+  Language: "English",
+  Country: "UK, USA",
+  Awards: "7 wins & 31 nominations.",
+  Poster: "http://ia.media-imdb.com/images/M/MV5BMTQ1MjE5MzU2M15BMl5BanBnXkFtZTgwNzE4OTMzMTE@._V1_SX300.jpg",
+  Metascore: "81",
+  imdbRating: "7.1",
+  imdbVotes: "86,600",
+  imdbID: "tt2692904",
+  Type: "movie",
+  Response: "True"
+}, {
+  Title: "The Rocky Horror Picture Show",
+  Year: "1975",
+  Rated: "R",
+  Released: "15 Aug 1975",
+  Runtime: "100 min",
+  Genre: "Comedy, Musical",
+  Director: "Jim Sharman",
+  Writer: "Richard O'Brien (original musical play), Jim Sharman (screenplay), Richard O'Brien (screenplay)",
+  Actors: "Tim Curry, Susan Sarandon, Barry Bostwick, Richard O'Brien",
+  Plot: "A newly engaged couple have a breakdown in an isolated area and must pay a call to the bizarre residence of Dr. Frank-N-Furter.",
+  Language: "English",
+  Country: "UK, USA",
+  Awards: "2 wins & 4 nominations.",
+  Poster: "http://ia.media-imdb.com/images/M/MV5BMjE5NDc0NjE2NF5BMl5BanBnXkFtZTcwMTE4MTI3NA@@._V1_SX300.jpg",
+  Metascore: "58",
+  imdbRating: "7.4",
+  imdbVotes: "100,437",
+  imdbID: "tt0073629",
+  Type: "movie",
+  Response: "True"
+}, {
+  Title: "Blade Runner",
+  Year: "1982",
+  Rated: "R",
+  Released: "25 Jun 1982",
+  Runtime: "117 min",
+  Genre: "Sci-Fi, Thriller",
+  Director: "Ridley Scott",
+  Writer: "Hampton Fancher (screenplay), David Webb Peoples (screenplay), Philip K. Dick (novel)",
+  Actors: "Harrison Ford, Rutger Hauer, Sean Young, Edward James Olmos",
+  Plot: "A blade runner must pursue and try to terminate four replicants who stole a ship in space and have returned to Earth to find their creator.",
+  Language: "English, German, Cantonese, Japanese, Hungarian",
+  Country: "USA, Hong Kong, UK",
+  Awards: "Nominated for 2 Oscars. Another 11 wins & 16 nominations.",
+  Poster: "http://ia.media-imdb.com/images/M/MV5BMTA4MDQxNTk2NDheQTJeQWpwZ15BbWU3MDE2NjIyODk@._V1_SX300.jpg",
+  Metascore: "88",
+  imdbRating: "8.2",
+  imdbVotes: "449,169",
+  imdbID: "tt0083658",
+  Type: "movie",
+  Response: "True"
+}, {
+  Title: "Fight Club",
+  Year: "1999",
+  Rated: "R",
+  Released: "15 Oct 1999",
+  Runtime: "139 min",
+  Genre: "Drama",
+  Director: "David Fincher",
+  Writer: "Chuck Palahniuk (novel), Jim Uhls (screenplay)",
+  Actors: "Edward Norton, Brad Pitt, Meat Loaf, Zach Grenier",
+  Plot: "An insomniac office worker, looking for a way to change his life, crosses paths with a devil-may-care soap maker, forming an underground fight club that evolves into something much, much more...",
+  Language: "English",
+  Country: "USA, Germany",
+  Awards: "Nominated for 1 Oscar. Another 9 wins & 30 nominations.",
+  Poster: "http://ia.media-imdb.com/images/M/MV5BMjIwNTYzMzE1M15BMl5BanBnXkFtZTcwOTE5Mzg3OA@@._V1_SX300.jpg",
+  Metascore: "66",
+  imdbRating: "8.9",
+  imdbVotes: "1,302,260",
+  imdbID: "tt0137523",
+  Type: "movie",
+  Response: "True"
+}, {
+  Title: "TRON",
+  Year: "1982",
+  Rated: "PG",
+  Released: "09 Jul 1982",
+  Runtime: "96 min",
+  Genre: "Action, Adventure, Sci-Fi",
+  Director: "Steven Lisberger",
+  Writer: "Steven Lisberger (screenplay), Steven Lisberger (story), Bonnie MacBird (story)",
+  Actors: "Jeff Bridges, Bruce Boxleitner, David Warner, Cindy Morgan",
+  Plot: "A computer hacker is abducted into the digital world and forced to participate in gladiatorial games where his only chance of escape is with the help of a heroic security program.",
+  Language: "English",
+  Country: "USA",
+  Awards: "Nominated for 2 Oscars. Another 2 wins & 6 nominations.",
+  Poster: "http://ia.media-imdb.com/images/M/MV5BMTY0OTM4ODM2MF5BMl5BanBnXkFtZTgwMTI0NDIxMDE@._V1_SX300.jpg",
+  Metascore: "65",
+  imdbRating: "6.8",
+  imdbVotes: "92,542",
+  imdbID: "tt0084827",
+  Type: "movie",
+  Response: "True"
+}, {
+  Title: "Office Space",
+  Year: "1999",
+  Rated: "R",
+  Released: "19 Feb 1999",
+  Runtime: "89 min",
+  Genre: "Comedy",
+  Director: "Mike Judge",
+  Writer: "Mike Judge (Milton animated shorts), Mike Judge (screenplay)",
+  Actors: "Ron Livingston, Jennifer Aniston, David Herman, Ajay Naidu",
+  Plot: "Three company workers who hate their jobs decide to rebel against their greedy boss.",
+  Language: "English",
+  Country: "USA",
+  Awards: "2 nominations.",
+  Poster: "http://ia.media-imdb.com/images/M/MV5BOTA5MzQ3MzI1NV5BMl5BanBnXkFtZTgwNTcxNTYxMTE@._V1_SX300.jpg",
+  Metascore: "68",
+  imdbRating: "7.8",
+  imdbVotes: "196,241",
+  imdbID: "tt0151804",
+  Type: "movie",
+  Response: "True"
+}, {
+  Title: "The Big Lebowski",
+  Year: "1998",
+  Rated: "R",
+  Released: "06 Mar 1998",
+  Runtime: "117 min",
+  Genre: "Comedy, Crime",
+  Director: "Joel Coen, Ethan Coen",
+  Writer: "Ethan Coen, Joel Coen",
+  Actors: "Jeff Bridges, John Goodman, Julianne Moore, Steve Buscemi",
+  Plot: "´The Dude´ Lebowski, mistaken for a millionaire Lebowski, seeks restitution for his ruined rug and enlists his bowling buddies to help get it.",
+  Language: "English, German, Hebrew, Spanish",
+  Country: "USA, UK",
+  Awards: "4 wins & 16 nominations.",
+  Poster: "http://ia.media-imdb.com/images/M/MV5BMTQ0NjUzMDMyOF5BMl5BanBnXkFtZTgwODA1OTU0MDE@._V1_SX300.jpg",
+  Metascore: "69",
+  imdbRating: "8.2",
+  imdbVotes: "523,395",
+  imdbID: "tt0118715",
+  Type: "movie",
+  Response: "True"
+}];
+
+
+
+
+
+
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+app.use('/static', express.static('client'));
+app.use('/static', express.static('/desktop' + '/client'));
+
+
+ 
+
+
+app.get("/movies/name=:tagId", function (req, res) {
+    
+    
+  console.log(req.params.tagId);
+  if (finder().length>=0)
+    {
+       
+       var arr = new Array ();
+       arr= finder();
+       var json=JSON.stringify(arr);
+       res.send (json);
+    } else
+    {
+        res.send ("Error");
+    }
+       
+       
+    function finder ()
+{
+    var _stringAux = req.params.tagId;
+    console.log(_stringAux);
+    var _arrAux= new Array();
+    _arrAux = findMoviesByString (_stringAux);
+       console.log(_arrAux);
+       return _arrAux;   
+}
+
+function findMoviesByString (_aString)
+{
+    var _arr = new Array();
+     for(var i=0; i<movies.length; i++)
+     {
+         if (findByString(_aString, movies[i].Title))
+         {
+             _arr.push(movies[i]);
+         }
+         
+     }    
+    return _arr;
+}
+
+
+function findByString (_stringToSearch, _stringToFind)
+{
+    _stringToSearch = _stringToSearch.toLowerCase();
+    _stringToFind=_stringToFind.toLowerCase();
+    
+    for (var i=0; i<_stringToFind.length; i++)
+    {
+       if (_stringToFind.indexOf(_stringToSearch, i)!==-1) return true;
+       
+    }
+    return false;
+}
+
+   
+});
+
+app.get("/movies/director=:tagId", function (req, res) {
+    
+    
+  console.log(req.params.tagId);
+  if (finder().length>=0)
+    {
+       
+       var arr = new Array ();
+       arr= finder();
+       var json=JSON.stringify(arr);
+       res.send (json);
+    } else
+    {
+        res.send ("Error");
+    }
+       
+       
+    function finder ()
+{
+    var _stringAux = req.params.tagId;
+    console.log(_stringAux);
+    var _arrAux= new Array();
+    _arrAux = findMoviesByString (_stringAux);
+       console.log(_arrAux);
+       return _arrAux;   
+}
+
+function findMoviesByString (_aString)
+{
+    var _arr = new Array();
+     for(var i=0; i<movies.length; i++)
+     {
+         if (findByString(_aString, movies[i].Director))
+         {
+             _arr.push(movies[i]);
+         }
+         
+     }    
+    return _arr;
+}
+
+
+function findByString (_stringToSearch, _stringToFind)
+{
+    _stringToSearch = _stringToSearch.toLowerCase();
+    _stringToFind=_stringToFind.toLowerCase();
+    
+    for (var i=0; i<_stringToFind.length; i++)
+    {
+       if (_stringToFind.indexOf(_stringToSearch, i)!==-1) return true;
+       
+    }
+    return false;
+}
+
+   
+});
+
+
+
+
+
+
+
+app.get("/movies", function (req, res) {
+    
+       
+    console.log(movies);
+    var json =JSON.stringify(movies);
+    res.send( json );
+   
+});
+
+var server = app.listen(8081, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log("Example app listening at http://%s:%s", host, port);
+
+});
+
+
+
+app.get('*', function(req, res) {
+        res.sendfile('../Client/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+    });
+
+      
+   
